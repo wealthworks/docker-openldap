3dbabf522b2124fb4c69797e66fee65fafe06127
Liut <liutao@liut.cc>
2015-07-04 14:53:53 +0800

    fixes base init

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM alpine:3.2
 MAINTAINER Eagle Liut <eagle@dantin.me>
 
-RUN apk add --update openldap openldap-back-bdb openldap-clients && rm -rf /var/cache/apk/*
+RUN apk add --update bash openldap openldap-back-bdb openldap-back-hdb openldap-clients && rm -rf /var/cache/apk/*
 
 ADD slapd.conf /etc/openldap/slapd.conf
+ENV LDAPCONF /etc/openldap/slapd.conf
 
+ADD modules /etc/openldap/modules
 ADD entrypoint.sh /ep.sh
 
 ENTRYPOINT ["/ep.sh"]
@@ -13,4 +15,5 @@ VOLUME ["/var/lib/openldap"]
 
 EXPOSE 389
 
-CMD ["slapd", "-d", "32768", "-u", "ldap", "-g", "ldap"]
+# CMD ["/ep.sh"]
+CMD ["slapd", "-u", "ldap", "-g", "ldap", "-d", "32768"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # ulimit -n 8192
 
@@ -15,29 +15,65 @@ if [ ! -e "$FIRST_START_DONE" ]; then
         exit 1
     fi
 
-    if [[ -z "$SLAPD_BASE_DN" ]]; then
-        echo -n >&2 "Error: Container not configured and SLAPD_BASE_DN not set. "
-        echo >&2 "Did you forget to add -e SLAPD_BASE_DN=... ?"
+    if [[ -z "$SLAPD_DOMAIN" ]]; then
+        echo -n >&2 "Error: Container not configured and SLAPD_DOMAIN not set. "
+        echo >&2 "Did you forget to add -e SLAPD_DOMAIN=... ?"
         exit 1
     fi
 
-  if [[ -n "$SLAPD_BASE_DN" ]]; then
-    sed -i "s|dc=example,dc=net|$SLAPD_BASE_DN|g" /etc/openldap/slapd.conf
-    sed -i "s/^#BASE.*/${SLAPD_BASE_DN}/g" /etc/openldap/ldap.conf
-  fi
-
-  if [[ -n "$SLAPD_PASSWORD" ]]; then
-    sed -i "s|rootpw.*|rootpw        $SLAPD_PASSWORD|g" /etc/openldap/slapd.conf
-  fi
-
   if [[ ! -d /var/lib/openldap/data ]]; then
     mkdir -p /var/lib/openldap/data
-    # chown -R ldap:ldap /var/lib/openldap/data
-    cp /var/lib/openldap/openldap-data/DB_CONFIG.example /var/lib/openldap/data/DB_CONFIG
+    cp /etc/openldap/DB_CONFIG.example /var/lib/openldap/data/DB_CONFIG
+    chown -R ldap:ldap /var/lib/openldap/data
+    chmod 700 /var/lib/openldap/data
   fi
 
-  chown -R ldap:ldap /var/lib/openldap/
+  if [[ ! -d /etc/openldap/slapd.d ]]; then
+    mkdir /etc/openldap/slapd.d
+  fi
 
+  chown -R ldap:ldap /var/lib/openldap/ /etc/openldap/slapd.d
+
+  if [[ -n "$SLAPD_PASSWORD" ]]; then
+    password_hash=`slappasswd -s "${SLAPD_PASSWORD}"`
+    sed_safe_password_hash=${password_hash//\//\\\/}
+    sed -i "s|rootpw.*|rootpw        ${sed_safe_password_hash}|g" /etc/openldap/slapd.conf
+  fi
+
+  if [[ -n "$SLAPD_DOMAIN" ]]; then
+    SLAPD_ORGANIZATION="${SLAPD_ORGANIZATION:-${SLAPD_DOMAIN}}"
+
+    dc_string=""
+    IFS="."; declare -a dc_parts=($SLAPD_DOMAIN)
+    odc=""
+    for dc_part in "${dc_parts[@]}"; do
+      [ -z "$dc_string" ] && odc="$dc_part"
+        dc_string="$dc_string,dc=$dc_part"
+    done
+    base_string="${dc_string:1}"
+    echo "BASE: ${base_string}"
+    echo "dc: $odc"
+
+    sed -i "s|dc=example,dc=net|$base_string|g" /etc/openldap/slapd.conf
+    sed -i "s|dc=example,dc=net|$base_string|g" /etc/openldap/modules/base.ldif
+    sed -i "s|dc: example|dc: $odc|g" /etc/openldap/modules/base.ldif
+    sed -i "s|o: Example|o: $SLAPD_ORGANIZATION|g" /etc/openldap/modules/base.ldif
+    sed -i "s/^#BASE.*/BASE  ${base_string}/g" /etc/openldap/ldap.conf
+
+    slapd -u ldap -g ldap >/dev/null 2>&1
+
+    ldapadd -x -D "cn=admin,${base_string}" -w "${SLAPD_PASSWORD}" -f /etc/openldap/modules/base.ldif
+
+    killall slapd
+
+    # slapadd -n0 -l /etc/openldap/modules/base.ldif >/dev/null 2>&1
+  fi
+
+  if [[ -f /etc/conf.d/slapd ]]; then
+    sed -i "s|#OPTS=.*|OPTS=\"-F /etc/openldap/slapd.d -h 'ldap:// ldapi://%2fvar%2frun%2fopenldap%2fslapd.sock'\"|g" /etc/conf.d/slapd
+  fi
+
+  touch $FIRST_START_DONE
 else
     slapd_configs_in_env=`env | grep 'SLAPD_'`
 
@@ -46,4 +82,5 @@ else
     fi
 fi
 
+echo "exec $@"
 exec "$@"
